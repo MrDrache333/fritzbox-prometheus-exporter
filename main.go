355d13612bf612b0a9423ced3d6a6569603e1f09
Keno <keno@oelrichsgarcia.de>
2025-01-14 22:31:34 +0100

    Update main.go

--- main.go
+++ main.go
@@ -33,8 +33,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
-	lua "github.com/sberk42/fritzbox_exporter/fritzbox_lua"
-	upnp "github.com/sberk42/fritzbox_exporter/fritzbox_upnp"
+	lua "github.com/mrdrache333/fritzbox-prometheus-exporter/fritzbox_lua"
+	upnp "github.com/mrdrache333/fritzbox-prometheus-exporter/fritzbox_upnp"
 )
 
 const serviceLoadRetryTime = 1 * time.Minute
